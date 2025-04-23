5143aeb9d99770d3713bb7483c0e71f0af7072eb
Aditya <adityacodesinpython@gmail.com>
2025-04-23 23:07:18 +0530

    2

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+// service-worker.js
+const CACHE_NAME = 'v1';
+const CACHE_ASSETS = [
+  'index.html',
+  'styles.css',
+  'logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(CACHE_ASSETS);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
